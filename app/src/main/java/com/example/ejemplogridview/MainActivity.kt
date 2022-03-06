0a532a954ab42872db220c7419af32b7d79fc272
HildaPech <72896837+HildaPech@users.noreply.github.com>
2022-03-06 06:28:09 -0500

    GridView con adaptador simple

--- app/src/main/java/com/example/ejemplogridview/MainActivity.kt
+++ app/src/main/java/com/example/ejemplogridview/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.ejemplogridview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.GridView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var frutas = ArrayList<String>()
+        frutas.add("Manzana")
+        frutas.add("Durazno")
+        frutas.add("Plátano")
+        frutas.add("Sandía")
+
+        val grid:GridView = findViewById(R.id.grid)
+        val adaptador = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, frutas)
+        grid.adapter = adaptador
+        grid.onItemClickListener = AdapterView.OnItemClickListener{ parent, view, position, id ->
+            Toast.makeText(this, frutas.get(position), Toast.LENGTH_LONG).show()}
+        }
     }
-}
